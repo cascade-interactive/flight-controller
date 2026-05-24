a66069272fa3d5081e2c7f8f293ffea2bad7d72d
cascade-interactive <the.studios2016@gmail.com>
2026-05-24 16:37:12 -0400

    Quaternion conversion function

--- Core/Inc/Math/CoreMath.hpp
+++ Core/Inc/Math/CoreMath.hpp
@@ -217,16 +217,6 @@ struct Vector3 {
 }
 
 // ------------------------------
-// Euler angles
-// ------------------------------
-
-struct EulerAngles {
-    float x;
-    float y;
-    float z;
-};
-
-// ------------------------------
 // Quaternion
 // ------------------------------
 
@@ -325,4 +315,24 @@ struct Quaternion {
                           (w * other.z) + (x * other.y) - (y * other.x) + (z * other.w));
     }
 };
+
+[[nodiscard]] inline Vector3 QuaternionToEuler(const Quaternion &q) {
+    Vector3 euler;
+
+    float sinr_cosp = 2.0f * (q.w * q.x + q.y * q.z);
+    float cosr_cosp = 1.0f - 2.0f * (q.x * q.x + q.y * q.y);
+    euler.x = std::atan2(sinr_cosp, cosr_cosp);
+
+    float sinp = 2.0f * (q.w * q.y - q.z * q.x);
+
+    sinp = Clamp(sinp, -1.0f, 1.0f);
+
+    euler.y = std::asin(sinp);
+
+    float siny_cosp = 2.0f * (q.w * q.z + q.x * q.y);
+    float cosy_cosp = 1.0f - 2.0f * (q.y * q.y + q.z * q.z);
+    euler.z = std::atan2(siny_cosp, cosy_cosp);
+
+    return euler;
+}
 } // namespace CoreMath
