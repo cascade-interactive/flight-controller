b4abcb506559f8d338106c17405eafe45d5ce622
cascade-interactive <the.studios2016@gmail.com>
2026-05-23 21:27:43 -0400

    CPP to C bridge

---
+++ Core/Inc/main_bridge.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void flight_controller_run(void);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Core/Src/main_bridge.cpp
@@ -0,0 +1,15 @@
+/*
+ * main_bridge.cpp
+ *
+ *  Created on: May 23, 2026
+ *      Author: Alexander
+ */
+
+#include "main_bridge.h"
+#include "CoreMath.hpp"
+#include "imu_icm20602.hpp"
+
+void flight_controller_run(void) {
+    while (1) {
+    }
+}
