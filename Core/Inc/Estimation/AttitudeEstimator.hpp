c7030210d9045c082b1c22c3122f65611fd3b678
cascade-interactive <the.studios2016@gmail.com>
2026-05-24 17:52:05 -0400

    Prepared Estimator

--- Core/Inc/Estimation/AttitudeEstimator.hpp
+++ Core/Inc/Estimation/AttitudeEstimator.hpp
@@ -1,5 +1,5 @@
 /*
- * imu_icm20602_registers.hpp
+ * AttitudeEstimator.hpp
  *
  *  Created on: May 23, 2026
  *      Author: Alexander
@@ -7,6 +7,20 @@
 
 #pragma once
 #include "CoreMath.hpp"
-#define CM CoreMath
 
-class AttitudeEstimator {};
+class AttitudeEstimator {
+  public:
+    AttitudeEstimator() = default;
+
+    // Input
+    void update(CoreMath::Vector3 gyro_rad_s, CoreMath::Vector3 accel_g, float dt_s);
+
+    // Output
+    CoreMath::Vector3 get_euler_attitude() const;
+
+  private:
+    // State
+    CoreMath::Quaternion current_attitude{CoreMath::Quaternion::Identity()};
+
+    // TODO: Covariance matrices for EKF
+};
