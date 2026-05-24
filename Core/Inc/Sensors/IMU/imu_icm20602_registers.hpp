b64022035876db7d69b2335c0d7f60036e543387
cascade-interactive <the.studios2016@gmail.com>
2026-05-23 20:34:04 -0400

    Fixed includes

--- Core/Inc/Sensors/IMU/imu_icm20602_registers.hpp
+++ Core/Inc/Sensors/IMU/imu_icm20602_registers.hpp
@@ -13,25 +13,24 @@
 
 // Register definitions
 namespace ICM20602 {
-	namespace Reg {
-		inline constexpr uint8_t WHO_AM_I		= 0x75;
-		inline constexpr uint8_t CONFIG			= 0x1A;	// Page 36
+namespace Reg {
+inline constexpr uint8_t WHO_AM_I = 0x75;
+inline constexpr uint8_t CONFIG = 0x1A; // Page 36
 
-		inline constexpr uint8_t GYRO_CONFIG	= 0x1B;
+inline constexpr uint8_t GYRO_CONFIG = 0x1B;
 
-		inline constexpr uint8_t ACCEL_CONFIG	= 0x1C;
-		inline constexpr uint8_t ACCEL_CONFIG_2	= 0x1D;
+inline constexpr uint8_t ACCEL_CONFIG = 0x1C;
+inline constexpr uint8_t ACCEL_CONFIG_2 = 0x1D;
 
-		inline constexpr uint8_t PWR_MGMT_1		= 0x6B; // Page 46
-		inline constexpr uint8_t PWR_MGMT_2		= 0x6C;
+inline constexpr uint8_t PWR_MGMT_1 = 0x6B; // Page 46
+inline constexpr uint8_t PWR_MGMT_2 = 0x6C;
 
-		inline constexpr uint8_t I2C_IF			= 0x70;	// Bit 4 disables I2C
+inline constexpr uint8_t I2C_IF = 0x70; // Bit 4 disables I2C
 
-
-		// Readings
-		inline constexpr uint8_t GYRO_XOUT_H	= 0x43; // 6 bytes, spanning 0x43 to 0x48
-		inline constexpr uint8_t ACCEL_XOUT_H	= 0x3B; // 6 bytes, spanning 0x3B to 0x40
-	}
-}
+// Readings
+inline constexpr uint8_t GYRO_XOUT_H = 0x43;  // 6 bytes, spanning 0x43 to 0x48
+inline constexpr uint8_t ACCEL_XOUT_H = 0x3B; // 6 bytes, spanning 0x3B to 0x40
+} // namespace Reg
+} // namespace ICM20602
 
 #endif /* INC_SENSORS_IMU_IMU_ICM20602_REGISTERS_HPP_ */
--- Core/Inc/Sensors/IMU/imu_icm20602_spi.hpp
+++ Core/Inc/Sensors/IMU/imu_icm20602_spi.hpp
@@ -4,12 +4,24 @@
  *  Created on: May 23, 2026
  *      Author: Alexander
  */
+#pragma once
 
-#ifndef INC_SENSORS_IMU_IMU_ICM20602_SPI_HPP_
-#define INC_SENSORS_IMU_IMU_ICM20602_SPI_HPP_
+#include "CoreMath.hpp"
+#include "imu_icm20602.hpp"
 
+ICM20602_IMU::ICM20602_IMU(SPI_HandleTypeDef *hspi, GPIO_TypeDef *cs_port,
+                           uint16_t cs_pin)
+    : _hspi(hspi), _cs_port(cs_port), _cs_pin(cs_pin) {
 
+  // Clear on startup
+  gyro = CoreMath::Vector3::Zero();
+  accel = CoreMath::Vector3::Zero();
+}
 
+void ICM20602_IMU::select_cs() {
+  HAL_GPIO_WritePin(_cs_port, _cs_pin, GPIO_PIN_RESET);
+}
 
-
-#endif /* INC_SENSORS_IMU_IMU_ICM20602_SPI_HPP_ */
+void ICM20602_IMU::deselect_cs() {
+  HAL_GPIO_WritePin(_cs_port, _cs_pin, GPIO_PIN_SET);
+}
